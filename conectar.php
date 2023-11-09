914ef631d514f872bb9b264399e4b2de88b7a48b
Luisa Andrielly <luisa.2021315963@aluno.iffar.edu.br>
2023-11-09 14:38:54 -0300

    Create conectar.php

---
+++ conectar.php
@@ -0,0 +1,21 @@
+<?php
+
+function conectar(){
+
+    $host = "localhost";
+    $user = "root";
+    $pass= "";
+    $db = "cadeira";
+    
+    $conexao= mysqli_connect($host,$user,$pass,$db);
+    
+    if($conexao){
+        return $conexao;
+    } else{
+        die("Problemas ao conectar no banco de dados. Erro;" . mysqli_connect_errno()."".mysqli_connect_error());
+    }
+    
+    }
+    
+
+?>
